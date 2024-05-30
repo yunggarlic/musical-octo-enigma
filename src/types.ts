ac819e14b5686465af599c0a7ac272d755a9b788
Tim Ferrari <ferrari.tim.v@gmail.com>
2024-05-30 09:41:10 -0500

    adds geo and weather types

--- src/types.ts
+++ src/types.ts
@@ -1,10 +1,17 @@
 import { Request } from "express";
 
-export interface WeatherRequestQuery {
+export interface GeolocationRequestQuery {
   city: string;
   state?: string;
   country?: string;
   limit?: string;
+}
+
+export type GeolocationRequest = Request<{}, {}, {}, GeolocationRequestQuery>;
+
+export interface WeatherRequestQuery {
+  lat: string;
+  lon: string;
 }
 
 export type WeatherRequest = Request<{}, {}, {}, WeatherRequestQuery>;
