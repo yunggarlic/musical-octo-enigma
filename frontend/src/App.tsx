d1286f2c237b9fe7e4037a01d8c4ef9c06e766f2
Tim <ferrari.tim.v@gmail.com>
2024-05-30 22:58:32 -0500

    consolidates markup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,16 +1,8 @@
-import { useState } from "react";
 import "./App.css";
 import { WeatherSearch } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <div>
-      <h1>Weather Search</h1>
-      <WeatherSearch />
-    </div>
-  );
+  return <WeatherSearch />;
 }
 
 export default App;
